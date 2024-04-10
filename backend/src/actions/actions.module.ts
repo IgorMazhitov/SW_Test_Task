d1f281861f649db3a3c7d8ca0cfe7ba8a5448086
Igor Mazhitov <mazhitov.igo@mail.ru>
2024-04-10 20:30:50 +0200

    5.6 Imports Fix

--- backend/src/actions/actions.module.ts
+++ backend/src/actions/actions.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ActionsController } from './actions.controller';
 import { ActionsService } from './actions.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './database/item.entity';
+import { Action } from './database/action.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ActionsController],
-  providers: [ActionsService]
+  providers: [ActionsService],
+  imports: [
+    TypeOrmModule.forFeature([Item, Action]), forwardRef(() => AuthModule)
+  ]
 })
 export class ActionsModule {}
