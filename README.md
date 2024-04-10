8f554cb375168a323d9fea66f2507f904e1195b5
Igor Mazhitov <mazhitov.igo@mail.ru>
2024-04-10 20:37:20 +0200

    Aux Commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # One-More
 Stranger Things
+
+## 1
+### - docker compose build
+### - docker compose up
+### - everything is ready to be tested - FE and BE and DB are running
+### BE port is - 3300
+### FE port is - 3000
--- backend/src/users/dtos/change-user.dto.ts
+++ backend/src/users/dtos/change-user.dto.ts
@@ -1,16 +1,18 @@
-import { IsEmail, IsNumber, IsString, Length } from "class-validator";
+import { IsEmail, IsNumber, IsString, Length } from 'class-validator';
 
 export class ChangeUserDto {
-    @IsNumber({}, { message: 'User Id should be number' })
-    readonly id: number;
-    @IsString({ message: 'User name has to be a string'})
-    readonly userName: string;
-    @IsString({ message: 'User email has to be a string'})
-    @IsEmail({}, { message: 'User email is invalid' })
-    readonly email: string;
-    @IsString({ message: 'User password has to be a string'})
-    @Length(4, 16, { message: 'User password should be more than 4 digits and less than 16' })
-    readonly password: string;
-    @IsNumber({}, { message: 'Role Id should be number' })
-    readonly roleId: number;
+  @IsNumber({}, { message: 'User Id should be number' })
+  readonly id: number;
+  @IsString({ message: 'User name has to be a string' })
+  readonly userName: string;
+  @IsString({ message: 'User email has to be a string' })
+  @IsEmail({}, { message: 'User email is invalid' })
+  readonly email: string;
+  @IsString({ message: 'User password has to be a string' })
+  @Length(4, 16, {
+    message: 'User password should be more than 4 digits and less than 16',
+  })
+  readonly password: string;
+  @IsNumber({}, { message: 'Role Id should be number' })
+  readonly roleId: number;
 }
