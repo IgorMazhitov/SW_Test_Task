fb26a7b698699352caf94f992262cc8a7305b52e
Igor Mazhitov <mazhitov.igo@mail.ru>
2024-04-10 16:35:13 +0200

    Second commit Docker/Logs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,16 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'scorewarriors',
-      password: 'defaultpassword',
-      database: 'scorewarriors',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       entities: [],
       synchronize: true,
       autoLoadEntities: true
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,8 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const appPort = process.env.PORT || 3000
+  const appPort = process.env.PORT || 3300
+  const appEnv = process.env.NODE_ENV
   const app = await NestFactory.create(AppModule);
-  await app.listen(appPort, () => console.log('Application started, ScoreWarriors - Welcome to the Admin'));
+  await app.listen(appPort, () => 
+    console.log(
+      `
+        Application started, ScoreWarriors - Welcome to the Admin
+        PORT IS = ${appPort} and ENV IS = ${appEnv}
+      `
+    )
+  );
 }
 bootstrap();
