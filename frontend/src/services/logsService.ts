57ecd109c02cc950e2ee21a145f4258bfcd62c6f
Igor Mazhitov <mazhitov.igo@mail.ru>
2024-05-03 12:38:16 +0200

    Audit Service Fix

--- frontend/src/services/logsService.ts
+++ frontend/src/services/logsService.ts
@@ -3,8 +3,10 @@ import $api from '../http'
 import { AuditResponse, GetAllLogsParams, IAudit } from '../interfaces/IAudit'
 
 export default class LogsService {
-    static async getAllLogs(request: GetAllLogsParams): Promise<AxiosResponse<AuditResponse>> {
+    static async getAllLogs(request: GetAllLogsParams): Promise<AuditResponse> {
         const { email, page = 1, limit = 10 } = request;
-        return $api.post<AuditResponse>('/logs/all', { email, page, limit })
+        const response = await $api.post<AuditResponse>('/logs/all', { email, page, limit })
+        const { logs, total } = response.data
+        return { logs, total }
     }
 }
